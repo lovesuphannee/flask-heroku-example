3a89c7add37b693b5d9a1f7b59d83d738c90c7ff
lovesuphannee <51392698+lovesuphannee@users.noreply.github.com>
2019-06-12 10:00:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
-
+    #json_data = {'Hello': 'World!'}
+   # return jsonify(json_data)
+    return "น.ส.สุพรรณี ศรีษะ เลขที่ 16 ม.4/1"
 
 if __name__ == '__main__':
     app.run()
